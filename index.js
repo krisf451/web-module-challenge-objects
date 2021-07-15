ef7104fff97955f72edd85f37cae851dd183addc
Kristian Fulkerson <kristianf451@gmail.com>
2021-07-15 13:13:58 -0700

    fixed mvp.test.js, finished task 1

--- index.js
+++ index.js
@@ -19,8 +19,8 @@ The function should:
   Example createMenuItem('tacos', 8, 'Lunch') should return {name: 'tacos', price: 8, category: 'Lunch'}
 */
 
-function createMenuItem(/*Your code here*/) {
-  /*Your code here*/
+function createMenuItem(val1, val2, val3) {
+  return { name: val1, price: val2, category: val3 };
 }
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 1b: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀
@@ -32,6 +32,12 @@ Test your createMenuItems function by doing the following:
   
   For example: createMenuItem("pizza",5,"lunch") would return this as the object: {name:"Pizza",price:5,category:"lunch"}
 */
+console.log("task1b object1", createMenuItem("pizza", 5, "lunch"));
+console.log("task1b object2", createMenuItem("hot sandwich", 3.5, "lunch"));
+console.log(
+  "task1b object3",
+  createMenuItem("cheeseburger", 6.5, "lunch/dinner")
+);
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 2: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀
 You're having a lunch special! 25% off for teachers and students, 10% off for everyone else. Add a method to the 
